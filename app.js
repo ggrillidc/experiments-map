fa62b36d7818222a47041f76a8d3abda97f829ce
ggrillidc <ggrillidc@gmail.com>
2024-02-04 00:20:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 // Initialize the map
 const map = L.map('map').setView([0, 0], 2);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('https://raw.githubusercontent.com/cajohare/AxionLimits/master/plots/Wavy_Map.png').addTo(map);
