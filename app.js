6034453e99fd9e07094628184b3eb7a63629ce14
ggrillidc <ggrillidc@gmail.com>
2024-12-10 09:25:45 +0100

    update

--- app.js
+++ app.js
@@ -829,7 +829,7 @@ experiments.forEach((experiment, index) => {
 
   // Create a marker with a custom HTML element as its content
   const marker = L.marker(markerPosition, { opacity: 1.0, icon: coloredTextMarker }).addTo(map);
-  marker.bindPopup(`<b>${experiment.name}</b><br>${experiment.description}<br><a href="${experiment.link}" target="_blank">Experiment Page</a>`);
+  //marker.bindPopup(`<b>${experiment.name}</b><br>${experiment.description}<br><a href="${experiment.link}" target="_blank">Experiment Page</a>`);
 
   // Add MathJax LaTeX formula to the popup description
   const popupContent = `
